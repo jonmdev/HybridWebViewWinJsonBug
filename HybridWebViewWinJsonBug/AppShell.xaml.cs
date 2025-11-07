d413558c48024d3cdffba247a75d8d1532a81b84
jonmdev <43948450+jonmdev@users.noreply.github.com>
2025-11-07 15:45:31 -0500

    Initial commit

---
+++ HybridWebViewWinJsonBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HybridWebViewWinJsonBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
